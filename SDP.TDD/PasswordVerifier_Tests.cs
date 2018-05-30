86927a9ac9adb8f73ad989365d3132786faf3d64
Marcin Przybylski <marcin.koszalin@gmail.com>
2018-05-30 21:29:16 +0200

    [Fail] Not larger than 8 throws

--- SDP.TDD/PasswordVerifier_Tests.cs
+++ SDP.TDD/PasswordVerifier_Tests.cs
@@ -1,11 +1,21 @@
 ﻿using NUnit.Framework;
+using System;
 
 namespace SDP.TDD
 {
     [TestFixture]
     public class PasswordVerifier_Tests
     {
-        
+        [Test]
+        public void Verify_WhenPasswordNotLargerThan8_Throws()
+        {
+            var passwordVerifier = new PasswordVerifier();
+            var password = "12345678";
+
+            Action<string> verify = passwordVerifier.Verify;
+
+            Assert.Throws<Exception>(() => verify(password));
+        }   
     }
 
     public class PasswordVerifier
